76fabe6a18b71972a1a92a48d6e54c9f542b8283
SeolYoungKim <nasur4da@gmail.com>
2024-01-07 14:06:34 +0900

    feat: InnerClass 필드 관련 학습

--- src/main/java/nestedclass/OuterClass.java
+++ src/main/java/nestedclass/OuterClass.java
@@ -11,6 +11,15 @@ public class OuterClass {
         System.out.println(StaticNestedClass.staticNestedStaticField);
     }
 
+    void innerClass() {
+        System.out.println(InnerClass.innerStaticField);
+
+        // 당연히 그냥 참조는 안된다. InnerClass가 인스턴스화 되지 않았으니까. InnerClass는 OuterClass의 인스턴스화가 필수이므로 참조가 되는 것이다.
+//        System.out.println(innerField);
+        InnerClass innerClass = new InnerClass();
+        System.out.println(innerClass.innerField);  // 인스턴스화를 해야만 참조가 가능하다~
+    }
+
     static class StaticNestedClass {
         private static final int staticNestedStaticField = 10;
 
@@ -23,6 +32,9 @@ public class OuterClass {
     }
 
     class InnerClass {
+        private static final int innerStaticField = 10;
+        private final int innerField = 10;
+
         public InnerClass() {
             // static nested class와 달리 outerField를 참조할 수 있다.
             // InnerClass는 OuterClass의 인스턴스가 있어야만 생성할 수 있기 때문이다.
