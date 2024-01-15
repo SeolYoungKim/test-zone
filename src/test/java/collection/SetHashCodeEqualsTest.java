2d6047139eb849a9f917da08db7dc60cb9f36210
SeolYoungKim <nasur4da@gmail.com>
2024-01-15 21:49:25 +0900

    test: Set은 hashCode와 equals가 모두 구현되어 있어야만 정상 동작한다

---
+++ src/test/java/collection/SetHashCodeEqualsTest.java
@@ -0,0 +1,81 @@
+package collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetHashCodeEqualsTest {
+    @DisplayName("HashCode만 구현된 경우, HashCode가 서로 같아도, Set에서 두 객체는 서로 다른 객체로 취급되어 둘 다 저장된다.")
+    @Test
+    void onlySameHashCode() {
+        // given
+        OnlySameHashCode a = new OnlySameHashCode(1);
+        OnlySameHashCode b = new OnlySameHashCode(1);
+
+        // when
+        Set<OnlySameHashCode> set = new HashSet<>();
+        set.add(a);
+        set.add(b);
+
+        // then
+        assertThat(set).hasSize(2);
+    }
+
+    @DisplayName("HashCode와 Equals가 모두 구현돼야 두 객체가 서로 같은 객체로 취급되어 Set에 하나만 저장된다.")
+    @Test
+    void sameHashCodeAndEquals() {
+        // given
+        SameHashCodeAndEquals a = new SameHashCodeAndEquals(1);
+        SameHashCodeAndEquals b = new SameHashCodeAndEquals(1);
+
+        // when
+        Set<SameHashCodeAndEquals> set = new HashSet<>();
+        set.add(a);
+        set.add(b);
+
+        // then
+        assertThat(set).hasSize(1);
+    }
+
+    private static class OnlySameHashCode {
+        private final int id;
+
+        public OnlySameHashCode(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public int hashCode() {
+            return id;
+        }
+    }
+
+    private static class SameHashCodeAndEquals {
+        private final int id;
+
+        public SameHashCodeAndEquals(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            SameHashCodeAndEquals that = (SameHashCodeAndEquals) o;
+            return id == that.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+    }
+}
