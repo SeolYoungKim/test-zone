c75c12fb615d24516ae547ec11acc1a254a28e68
SeolYoungKim <nasur4da@gmail.com>
2024-01-21 16:02:51 +0900

    feat: notifyAll 학습

--- src/main/java/thread/WaitNotifyTest.java
+++ src/main/java/thread/WaitNotifyTest.java
@@ -31,23 +31,27 @@ public class WaitNotifyTest {
     public static void main(String[] args) {
         Object lock = new Object();
         MyThread myThread = new MyThread(lock);
+        MyThread myThread2 = new MyThread(lock);
 
         try {
             System.out.println("thread state=" + myThread.getState());
             myThread.start();  // 메인 스레드가 myThread를 실행시키면, myThread 내부에서 lock.wait()을 호출하여, myThread가 대기 상태가 된다.
+            myThread2.start();
             System.out.println("thread state(after start)=" + myThread.getState());  // 메인 스레드가 myThread의 상태 확인
 
             Thread.sleep(100);
             System.out.println("thread state(after 0.1sec)=" + myThread.getState());  // 메인 스레드가 myThread의 상태 확인
 
             synchronized (lock) {  // 메인 스레드가 lock의 모니터를 획득 (myThread 내부에서 wait()을 호출하여 락을 놓아야 가능)
-                lock.notify();  // lock의 모니터에서 대기중인 myThread를 깨운다.
+                lock.notifyAll();  // lock의 모니터에서 대기중인 myThread를 깨운다.
             }
             Thread.sleep(100);
             System.out.println("thread state(after notify)=" + myThread.getState());  // myThread가 깨어나고 나머지 작업들이 수행된다
 
             myThread.join();
             System.out.println("thread state(after join)=" + myThread.getState());
+            myThread2.join();
+            System.out.println("thread2 state(after join)=" + myThread2.getState());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
